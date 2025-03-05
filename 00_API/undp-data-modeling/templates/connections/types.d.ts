4602a0951e28a8e859acf3258244cce3bb905cfc
Ben-Keller <ben.ch.keller@gmail.com>
2025-03-05 13:03:46 +0300

    add api schema

---
+++ 00_API/undp-data-modeling/templates/connections/types.d.ts
@@ -0,0 +1,18 @@
+type ConnectionBackModule = {
+  colorscheme: "light" | "dark";
+  content: {
+    intro: string;
+    title: string;
+    cta: string;
+  };
+};
+
+type ConnectionNextModule = {
+  colorscheme: "light" | "dark";
+  content: {
+    image: string;
+    intro: string;
+    title: string;
+    cta: string;
+  };
+};
---
+++ 00_API/undp-data-modeling/templates/images/types.d.ts
@@ -0,0 +1,20 @@
+type PhotoFullH = {
+  content: {
+    imageUrl: string;
+    caption: string;
+  };
+};
+
+type PhotoLowH = {
+  content: {
+    imageUrl: string;
+    caption: string;
+  };
+};
+
+type PhotoMidH = {
+  content: {
+    imageUrl: string;
+    caption: string;
+  };
+};
