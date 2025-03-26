5ff0ae458841c3a8b068c44b0506e7604826136b
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2025-03-26 12:24:37 +0100

    Chore: add a basic package scaffolding

---
+++ src/sea/__init__.py
@@ -0,0 +1,6 @@
+"""
+This package contains an ETL pipeline to process Figma designs and transform
+them into a standardised JSON format for the Sustainable Energy Academy.
+"""
+
+__version__ = "0.1.0a0"
