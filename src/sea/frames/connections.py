bfb993ae3d23ec071dcea9f53fd3d0c5e7f47710
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2025-04-16 22:05:42 +0200

    Feat(connections): add a connection back frame

--- src/sea/frames/connections.py
+++ src/sea/frames/connections.py
@@ -8,7 +8,7 @@ from ..nodes import Node
 from .base import FrameBase
 from .utils import Image
 
-__all__ = ["ConnectionNext"]
+__all__ = ["ConnectionNext", "ConnectionBack"]
 
 
 class ConnectionNext(FrameBase):
@@ -20,7 +20,7 @@ class ConnectionNext(FrameBase):
     intro: str
     title: str
     cta: str = Field(default="Start learning")
-    # next_lesson_id: str
+    next_lesson_id: str = Field(alias="nextLessonId")
 
     @classmethod
     def from_node(cls, node: Node) -> "ConnectionNext":
@@ -37,10 +37,37 @@ class ConnectionNext(FrameBase):
         ConnectionContent
             An instance of the ConnectionContent class populated with data from the node.
         """
+        assert (
+            node.name == "connection_next"
+        ), f"Expected connection_next node, got {node.name}"
+        image_node = node.select_node("GROUP", "image")
         return cls(
-            template_id="connection_next",
-            image=Image.from_node(node.select_node("GROUP", "image")),
+            template_id=node.name,
+            image=image_node,
             intro=node.select_node("TEXT", "intro").characters,
             title=node.select_node("TEXT", "title").characters,
             cta=node.select_node("TEXT", "cta").characters,
+            next_lesson_id="1.1.1",
+        )
+
+
+class ConnectionBack(FrameBase):
+    image: Image
+    intro: str
+    title: str
+    cta: str | None = None
+
+    @classmethod
+    def from_node(cls, node: Node) -> "ConnectionBack":
+        assert (
+            node.name == "connection_back"
+        ), f"Expected connection_back node, got {node.name}"
+        image_node = node.select_node("GROUP", "image")
+        cta_node = node.select_node("TEXT", "cta")
+        return cls(
+            template_id=node.name,
+            image=Image.from_node(image_node),
+            intro=node.select_node("TEXT", "intro").characters,
+            title=node.select_node("TEXT", "title").characters,
+            cta=(cta_node.characters if cta_node is not None else None),
         )
